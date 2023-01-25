2c29aebeecadbb8edb323d2094dab575f5f3f8ba
idiotdoomspiral <70673821+idiotdoomspiral@users.noreply.github.com>
2023-01-24 16:32:47 -0800

    this fix sucks and i hate it

--- cogs/system.py
+++ cogs/system.py
@@ -12,23 +12,27 @@ class SystemCog(commands.Cog):
 
     @commands.command(name="restartbot")
     async def restartbot(self, ctx):
-        logger.info(self.sys_role)
+        valid_role = 0
         for role in ctx.author.roles:
-            logger.info(f"{role} | {role.id}")
             if role.id == self.sys_role:
-                subprocess.run("/mnt/thumb/waffle/scripts/restart.sh", shell=True)
-                logger.info(f"{ctx.author} is restarting the bot.")
-                await ctx.send("Bot restarting!")
-            else:
-                await ctx.send("You're no bot boy!")
+                valid_role = 1
+        if valid_role == 1:
+            subprocess.run("/mnt/thumb/waffle/scripts/restart.sh", shell=True)
+            logger.info(f"{ctx.author} is restarting the bot.")
+            await ctx.send("Bot restarting!")
+        else:
+            await ctx.send("You're no bot boy!")
 
     @commands.command(name="gitupdate")
     async def gitupdate(self, ctx):
+        valid_role = 0
         for role in ctx.author.roles:
             if role.id == self.sys_role:
-                subprocess.run("/waffle/scripts/update.sh", shell=True)
-                logger.info(f"{ctx.author} is updating the bot.")
-                await ctx.send("Bot updating!")
+                valid_role = 1
+        if valid_role == 1:
+            subprocess.run("/waffle/scripts/update.sh", shell=True)
+            logger.info(f"{ctx.author} is updating the bot.")
+            await ctx.send("Bot updating!")
         else:
             await ctx.send("You're no bot boy!")
 
