5817a81f7be882ff72c915d5eec7c2d5c87e7ac0
idiotdoomspiral <70673821+idiotdoomspiral@users.noreply.github.com>
2023-02-06 05:53:17 -0800

    typo

--- cogs/debrid.py
+++ cogs/debrid.py
@@ -232,7 +232,7 @@ class DebridCog(commands.Cog):
     @commands.command(name="search", aliases=["rarbg"])
     async def search(self, ctx, *, input: str):
         logger.info(ctx.args)
-        logger.info(ctx.kargs)
+        logger.info(ctx.kwargs)
         logger.info(ctx.command)
         logger.info(ctx.invoked_with)
         results = torrents.search(input, sortBy="seeders", order="desc")
