1c7a22a54dc4324382779f772d54bdce21d30faf
idiotdoomspiral <70673821+idiotdoomspiral@users.noreply.github.com>
2023-02-06 13:05:32 -0800

    testing 123

--- cogs/chatbot.py
+++ cogs/chatbot.py
@@ -45,7 +45,10 @@ class ChatbotCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.author.id == 295285994607869953:
+        if (
+            message.author.id == 295285994607869953
+            or message.author.id == 439606776187584523
+        ):
             hl_cmd = self.bot.get_command("hogwarts")
             ctx = await self.bot.get_context(message)
             await ctx.invoke(hl_cmd)
