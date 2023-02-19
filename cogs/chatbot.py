5e58f20dabe9a3027e0216fd8f7587216aae747d
idiotdoomspiral <70673821+idiotdoomspiral@users.noreply.github.com>
2023-02-18 18:50:11 -0800

    whoops

--- cogs/chatbot.py
+++ cogs/chatbot.py
@@ -46,10 +46,10 @@ class ChatbotCog(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         async with ctx.typing():
-            if message.author.id == 295285994607869953:
-                hl_cmd = self.bot.get_command("hogwarts")
-                ctx = await self.bot.get_context(message)
-                await ctx.invoke(hl_cmd)
+            #if message.author.id == 295285994607869953:
+            #    hl_cmd = self.bot.get_command("hogwarts")
+            #    ctx = await self.bot.get_context(message)
+            #    await ctx.invoke(hl_cmd)
             elif message.content.startswith("@waffle") or (
                 len(message.mentions) > 0 and "waffle" in message.mentions[0].name
             ):
