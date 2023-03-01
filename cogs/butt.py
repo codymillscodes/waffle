b93aba17fe8ca7e561b3a57806f99342969f121a
idiotdoomspiral <70673821+idiotdoomspiral@users.noreply.github.com>
2023-03-01 23:54:03 +0000

    more tuning

--- cogs/butt.py
+++ cogs/butt.py
@@ -9,10 +9,12 @@ message_count = 0
 pause_count = 0
 
 
+
 class ButtCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.chance = 20
+        self.chance = 15
+        self.butt_count = 3
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -40,29 +42,40 @@ class ButtCog(commands.Cog):
                 and not "https://" in message.content
             ):
                 if random.randint(1, 100) <= 20:
+                    butts = 0
                     logger.info(f"Buttifying {message.content}")
+                    previous_word = ""
                     for word in words:
-                        if word.startswith(":"):
+                        if 'butt' in previous_word.lower():
+                            new_word = word
+                            continue
+                        elif word.startswith(":"):
                             new_word = word
                         elif "-" in word:
                             parts = word.split("-")
                             if (
                                 random.choice([True, False])
-                                and random.randint(1, 100) <= self.chance
+                                and random.randint(1, 100) <= self.chance and butts < self.butt_count
                             ):
                                 new_word = "butt-" + parts[1]
+                                butts += 1
                             else:
                                 new_word = parts[0] + "-butt"
+                                butts += 1
                         else:
                             if (
                                 p.plural(word) == word
-                                and random.randint(1, 100) <= self.chance - 5
+                                and random.randint(1, 100) <= self.chance - 5 and butts < self.butt_count
                             ):
                                 new_word = "butts"
-                            elif random.randint(1, 100) <= self.chance - 5:
+                                butts += 1
+                            elif random.randint(1, 100) <= self.chance - 5 and butts < self.butt_count:
                                 new_word = "butt"
+                                butts += 1
                             else:
                                 new_word = word
+
+                        previous_word = word
 
                         new_message += new_word + " "
                     new_message = new_message.rstrip()
