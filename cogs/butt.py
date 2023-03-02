ce9d7db664a0676c2f6539ddaeeb9361bbe740da
idiotdoomspiral <70673821+idiotdoomspiral@users.noreply.github.com>
2023-03-02 00:21:05 -0800

    big butt changes

--- cogs/butt.py
+++ cogs/butt.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 import random
 import inflect
 from loguru import logger
+from lib.buttwords import filter
 
 p = inflect.engine()
 
@@ -12,24 +13,38 @@ pause_count = 0
 class ButtCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.chance = 20
+        self.chance = 5
+        self.filter = filter
+        self.pause_count = 0
+
+    def buttify(self, message):
+        words = message.split()
+        logger.info(f"Buttifying {message.content}")
+        butt_num = random.randint(1, len(words) - 1)
+        while words[butt_num] in self.filter or words[butt_num].startswith(":"):
+            butt_num = random.randint(1, len(words) - 1)
+        mod_word = words[butt_num]
+        if "-" in mod_word:
+            parts = mod_word.split("-")
+            if random.choice([True, False]):
+                words[butt_num] = "butt-" + parts[1]
+            else:
+                words[butt_num] = parts[0] + "-butt"
+        else:
+            if p.plural(mod_word) == mod_word:
+                words[butt_num] = "butts"
+            else:
+                words[butt_num] = "butt"
+
+        return words.join(words)
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        global message_count, pause_count
-
         if message.author == self.bot.user:
             return
 
         if self.bot.user in message.mentions:
             return
-
-        if pause_count > 0:
-            pause_count -= 1
-            return
-
-        new_message = ""
-        words = message.content.split()
 
         if "roll tide" in message.content.lower():
             logger.info(f"Roll tide detected in {message.content}")
@@ -39,41 +54,20 @@ class ButtCog(commands.Cog):
                 not message.content.startswith("!")
                 and not "https://" in message.content
             ):
-                if random.randint(1, 100) <= 20:
-                    logger.info(f"Buttifying {message.content}")
-                    for word in words:
-                        if word.startswith(":"):
-                            new_word = word
-                        elif "-" in word:
-                            parts = word.split("-")
-                            if (
-                                random.choice([True, False])
-                                and random.randint(1, 100) <= self.chance
-                            ):
-                                new_word = "butt-" + parts[1]
-                            else:
-                                new_word = parts[0] + "-butt"
-                        else:
-                            if (
-                                p.plural(word) == word
-                                and random.randint(1, 100) <= self.chance - 5
-                            ):
-                                new_word = "butts"
-                            elif random.randint(1, 100) <= self.chance - 5:
-                                new_word = "butt"
-                            else:
-                                new_word = word
+                if (
+                    random.randint(1, 100) <= self.chance
+                    and self.pause_count == 0
+                    and "butt" not in message.content.lower()
+                    and len(message.content.split()) > 3
+                ):
+                    new_message = self.buttify(message)
 
-                        new_message += new_word + " "
-                    new_message = new_message.rstrip()
                     if new_message != message.content:
-                        logger.info(f"New message: {new_message}")
-                        logger.info(f"Original message: {message.content}")
+                        self.pause_count = random.randint(5, 15)
                         await message.channel.send(new_message)
-                        message_count += 1
-                        if message_count >= 3:  # randomly pause after every 3 messages
-                            pause_count = random.randint(1, 20)
-                            message_count = 0
+                else:
+                    if self.pause_count > 0:
+                        self.pause_count -= 1
 
 
 async def setup(bot):
