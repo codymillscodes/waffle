b469c3142f5615373ebfc6c41f76de919a6ea805
idiotdoomspiral <70673821+idiotdoomspiral@users.noreply.github.com>
2023-03-03 22:18:34 -0800

    uhhh

--- cogs/events.py
+++ cogs/events.py
@@ -11,10 +11,10 @@ class EventsCog(commands.Cog):
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
         if before.channel and after.channel:
-            twitch_channel = await self.bot.fetch_channel(TWITCH_CHANNEL)
-            await twitch_channel.send(embed=stream_embed(member.name))
             if before.self_stream != after.self_stream:
-                logger.info(f"{member.name} stopped streaming")
+                twitch_channel = await self.bot.fetch_channel(TWITCH_CHANNEL)
+                await twitch_channel.send(embed=stream_embed(member.name))
+                # logger.info(f"{member.name} stopped streaming")
 
 
 async def setup(bot):
