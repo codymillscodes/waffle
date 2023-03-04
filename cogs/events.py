d2489fb81b287a221c633e5ceee46d1f0c8dbeb0
idiotdoomspiral <70673821+idiotdoomspiral@users.noreply.github.com>
2023-03-03 20:56:43 -0800

    bleh

--- cogs/events.py
+++ cogs/events.py
@@ -5,18 +5,18 @@ class EventsCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Bot.event
-    async def on_voice_state_update(self, member, before, after):
-        if before.channel is None and after.channel is not None:
-            # Member joined voice channel
-            voice_client = await after.channel.connect()
-            # see if there is a stream
-            stream = voice_client.stream
-        if stream is not None:
-            print("There is a stream in voice channel {0.name}".format(after.channel))
-        else:
-            print("There is no stream in voice channel {0.name}".format(after.channel))
-        await voice_client.disconnect()
+    # @commands.Bot.event
+    # async def on_voice_state_update(self, member, before, after):
+    #     if before.channel is None and after.channel is not None:
+    #         # Member joined voice channel
+    #         voice_client = await after.channel.connect()
+    #         # see if there is a stream
+    #         stream = voice_client.stream
+    #     if stream is not None:
+    #         print("There is a stream in voice channel {0.name}".format(after.channel))
+    #     else:
+    #         print("There is no stream in voice channel {0.name}".format(after.channel))
+    #     await voice_client.disconnect()
 
 
 async def setup(bot):
