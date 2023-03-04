1eeafc38a372db34b9fe8574cc06c1336ff84f23
idiotdoomspiral <70673821+idiotdoomspiral@users.noreply.github.com>
2023-03-03 21:48:57 -0800

    test

--- cogs/events.py
+++ cogs/events.py
@@ -8,8 +8,13 @@ class EventsCog(commands.Cog):
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
         if before.channel and after.channel:
-            if before.self_stream and not after.self_stream:
-                await member.send("You stopped streaming.")
+            if (
+                before.self_stream
+                or before.self_video
+                and not after.self_stream
+                or after.self_video
+            ):
+                print("You stopped streaming.")
 
 
 async def setup(bot):
