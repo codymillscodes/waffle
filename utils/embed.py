747e8ba5bf5ffa9acbd4acdec488d1f94e2d45f9
idiotdoomspiral <70673821+idiotdoomspiral@users.noreply.github.com>
2023-03-08 09:43:24 -0800

    hmm

--- utils/embed.py
+++ utils/embed.py
@@ -184,7 +184,7 @@ def debrid_status(all_status):
 
 
 def download_ready(author, magnet, link=None):
-    embed = Embed(description=f"{author.mention}")
+    embed = Embed(description=f"<@{author}>")
     if link is None:
         link = f"{DEBRID_WEBDAV}magnets/{quote(magnet)}/"
     embed.add_field(
