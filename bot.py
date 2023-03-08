06d33a213862bd9284eceba52b4ebe27d60e2a27
idiotdoomspiral <70673821+idiotdoomspiral@users.noreply.github.com>
2023-03-08 09:28:51 -0800

    dbg

--- bot.py
+++ bot.py
@@ -126,7 +126,7 @@ class Waffle(commands.Bot):
                 logger.info(f"Checking: {id['task_id']}")
                 async with Conn() as resp:
                     r = await resp.get_json(Urls.DEBRID_DELAYED + str(id["task_id"]))
-                    logger.info(str(id["task_id"]))
+                    logger.info(Urls.DEBRID_DELAYED + str(id["task_id"]))
                     logger.debug(r)
 
                 if r["data"]["status"] == 2:
