406ea027372ac8dfa449a2036607eb59e24b0fb4
idiotdoomspiral <70673821+idiotdoomspiral@users.noreply.github.com>
2023-03-16 14:25:01 -0700

    comment

--- cogs/reco.py
+++ cogs/reco.py
@@ -1,16 +1,16 @@
-from discord.ext import commands
-from loguru import logger
+# from discord.ext import commands
+# from loguru import logger
 
-# from utils import DB, embed
+# # from utils import DB, embed
 
 
-class RecoCog(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+# class RecoCog(commands.Cog):
+#     def __init__(self, bot):
+#         self.bot = bot
 
-    # self.db = DB()
+#     # self.db = DB()
 
-    @commands.tree.command()
-    @commands.describe(one="one!")
-    async def add_reco(self, interaction: commands.Interaction, one: str):
-        await interaction.response.send_message("one is " + one)
+#     @commands.tree.command()
+#     @commands.describe(one="one!")
+#     async def add_reco(self, interaction: commands.Interaction, one: str):
+#         await interaction.response.send_message("one is " + one)
