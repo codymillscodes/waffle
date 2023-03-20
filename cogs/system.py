0d7c6366d6d6a661bed179e7b8abf7be754e49ba
idiotdoomspiral <70673821+idiotdoomspiral@users.noreply.github.com>
2023-03-19 19:27:55 -0700

    ??

--- cogs/system.py
+++ cogs/system.py
@@ -32,6 +32,7 @@ class SystemCog(commands.Cog):
     @commands.command(name="clear")
     async def clear(self, ctx):
         guild = ctx.guild
+        commands = await ctx.bot.tree.fetch_commands(guild=guild)
         ctx.bot.tree.clear_commands(guild=guild)
         synced = await ctx.bot.tree.sync(guild=guild)
         await ctx.reply(f"Synced {len(synced)} commands.", mention_author=False)
