4545a8d3bf02285345dcd46964f77641cd95d7e5
idiotdoomspiral <70673821+idiotdoomspiral@users.noreply.github.com>
2023-04-03 02:30:22 -0700

    strippin

--- utils/debrid.py
+++ utils/debrid.py
@@ -68,10 +68,10 @@ def eval_pick(pick):
     # need to return a list of numbers
     pick_list = []
     if pick.isdigit():
-        pick_list.append(int(pick) - 1).strip()
+        pick_list.append(int(pick.strip()) - 1)
     elif "-" in pick:
-        start, end = pick.split("-").strip()
-        pick_list.extend(range(int(start) - 1, int(end)))
+        start, end = pick.split("-")
+        pick_list.extend(range(int(start.strip()) - 1, int(end.strip())))
     elif "," in pick:
         pick_list.extend([int(x) - 1 for x in pick.replace(" ", "").split(",")])
     logger.info(pick_list)
