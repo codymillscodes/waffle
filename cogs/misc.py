48ba8dc87a8c9269905884f74f18182eba022e9c
idiotdoomspiral <70673821+idiotdoomspiral@users.noreply.github.com>
2023-04-02 20:41:07 -0700

    oops

--- cogs/misc.py
+++ cogs/misc.py
@@ -7,25 +7,25 @@ class MiscCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command("roll", description="Roll a dice"):
+    @commands.command(name="roll", description="Roll a dice")
     async def roll(self, ctx, dice: str):
         """Rolls a dice in NdN format."""
-        logger.info(f'{ctx.author} rolled {dice}')
+        logger.info(f"{ctx.author} rolled {dice}")
         min = 1
-        if 'd' not in dice and dice.isdigit():
+        if "d" not in dice and dice.isdigit():
             max = int(dice)
-        elif 'd' in dice:
-            min = int(dice.split('d')[0])
-            max = int(dice.split('d')[1])
+        elif "d" in dice:
+            min = int(dice.split("d")[0])
+            max = int(dice.split("d")[1])
         else:
-            await ctx.send('Invalid input')
+            await ctx.send("Invalid input")
             return
-        
+
         if min > max:
-            await ctx.send('Invalid input')
+            await ctx.send("Invalid input")
             return
-        
-        await ctx.send(f'You rolled a {randint(min, max)}')
+
+        await ctx.send(f"You rolled a {randint(min, max)}")
 
     def setup(bot):
         bot.add_cog(MiscCog(bot))
