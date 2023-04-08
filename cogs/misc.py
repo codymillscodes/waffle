ebf368ec26cbae863056912e75c58824cb0f6d12
idiotdoomspiral <70673821+idiotdoomspiral@users.noreply.github.com>
2023-04-08 16:52:00 -0700

    bugfix

--- cogs/misc.py
+++ cogs/misc.py
@@ -13,22 +13,20 @@ class MiscCog(commands.Cog):
         logger.info(f"{ctx.author} rolled {dice}")
         min = 1
         if "d" not in dice and dice.isdigit():
-            max = int(dice)
+            await ctx.send(f"You rolled a {randint(min, int(dice))}")
         elif "d" in dice:
             rolls = []
             num_to_roll = int(dice.split("d")[0])
             for i in range(num_to_roll):
                 rolls.append(randint(1, int(dice.split("d")[1])))
             if len(rolls) == 1:
-                await ctx.send(f"You rolled {rolls[0]}")
+                await ctx.send(f"You rolled a {rolls[0]}")
             # send rolls '1 + 1 + 1 = 3' instead of [1, 1, 1], send the rseult of each roll and the total
             elif len(rolls) > 1:
                 await ctx.send(f"You rolled {rolls} = {sum(rolls)}")
         else:
             await ctx.send("Invalid input")
             return
-
-        await ctx.send(f"You rolled a {randint(min, max)}")
 
 
 async def setup(bot):
