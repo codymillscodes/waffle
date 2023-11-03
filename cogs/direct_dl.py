7c237dfe1c5e0e36e38826186c828366068a8a80
idiotdoomspiral <70673821+idiotdoomspiral@users.noreply.github.com>
2023-11-03 12:36:21 -0700

    fix tt

--- cogs/direct_dl.py
+++ cogs/direct_dl.py
@@ -170,28 +170,28 @@ class DirectDLCog(commands.Cog):
             link = await get_tiktok_link(message.content)
             if link == 0:
                 await message.add_reaction("‼️")
-            tt_file = await download_tiktok_video(link)
-            logger.info(f"tt_file: {tt_file}")
-            if tt_file["status"] == 1:
-                file = discord.File(
-                    f"tiktok/{tt_file['fn']}.mp4", filename=f"{tt_file['fn']}.mp4"
-                )
-                tt_message = tt_file["url"].split("/")
-                tt_message = urllib.parse.unquote(
-                    tt_message[-1].replace(".480.mp4", "")
-                )
-                logger.info(f"tt_message: {tt_message}")
+                tt_file = await download_tiktok_video(link)
+                logger.info(f"tt_file: {tt_file}")
+                if tt_file["status"] == 1:
+                    file = discord.File(
+                        f"tiktok/{tt_file['fn']}.mp4", filename=f"{tt_file['fn']}.mp4"
+                    )
+                    tt_message = tt_file["url"].split("/")
+                    tt_message = urllib.parse.unquote(
+                        tt_message[-1].replace(".480.mp4", "")
+                    )
+                    logger.info(f"tt_message: {tt_message}")
 
-                await message.delete()
-                await message.channel.send(
-                    f"<@{message.author.id}>\n>>> {tt_message}", file=file
-                )
+                    await message.delete()
+                    await message.channel.send(
+                        f"<@{message.author.id}>\n>>> {tt_message}", file=file
+                    )
 
-                delete = await delete_file(f"tiktok/{tt_file['fn']}.mp4")
-                if delete:
-                    logger.info(f"Deleted {tt_file['fn']}.mp4")
-                else:
-                    logger.error(f"Failed to delete {tt_file['fn']}.p4")
+                    delete = await delete_file(f"tiktok/{tt_file['fn']}.mp4")
+                    if delete:
+                        logger.info(f"Deleted {tt_file['fn']}.mp4")
+                    else:
+                        logger.error(f"Failed to delete {tt_file['fn']}.mp4")
 
 
 async def setup(bot):
