75048b517d146752297c59d615625d75bf9a171e
idiotdoomspiral <70673821+idiotdoomspiral@users.noreply.github.com>
2024-03-11 13:10:45 -0700

    rethink how to send multiple

--- cogs/helldivers.py
+++ cogs/helldivers.py
@@ -11,12 +11,12 @@ class HelldiversCog(commands.Cog):
 
     @app_commands.command(name="hd2events", description="Get active Helldivers events")
     async def hd2events(self, interaction: discord.Interaction):
-        await interaction.response.defer(thinking=True)
+        # await interaction.response.defer(thinking=True)
         logger.info("hd2 events requested.")
         events = await utils.hd2.helldivers_events()
         for e in events:
             embed = discord.Embed(title=e["title"], description=e["message"])
-            await interaction.followup.send(embed=embed)
+            await interaction.response.send_message(embed=embed)
 
 
 async def setup(bot):
