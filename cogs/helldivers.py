757057f5f9bd96bd853ca729b8a345878da9a526
idiotdoomspiral <70673821+idiotdoomspiral@users.noreply.github.com>
2024-03-11 12:59:53 -0700

    helldivers command!

---
+++ cogs/helldivers.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+from loguru import logger
+import utils.hd2
+
+
+class HelldiversCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="hd2events", description="Get active Helldivers events")
+    async def hd2events(self, interaction: discord.Interaction):
+        await interaction.response.defer(thinking=True)
+        logger.info("hd2 events requested.")
+        events = await utils.hd2.helldivers_events()
+        for e in events:
+            embed = discord.Embed(title=e["title"], description=e["message"])
+            await interaction.followup.send(embed=embed, mention_author=False)
+
+
+async def setup(bot):
+    await bot.add_cog(HelldiversCog(bot))
---
+++ utils/hd2.py
@@ -0,0 +1,24 @@
+import httpx
+import json
+
+
+async def helldivers_events():  # events for now
+    url = "https://api.live.prod.thehelldiversgame.com/api/WarSeason/801/Status"
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url)
+
+    events = resp.json()["globalEvents"]
+    return events
+
+
+# async def main():
+#     events = await helldivers_events()
+#     for e in events:
+#         print(e["title"])
+#         print(e["message"])
+
+
+# if __name__ == "__main__":
+#     import asyncio
+
+#     asyncio.run(main())
